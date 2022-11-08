55c13def5988b44b597d954b99c0cc1cec5eb444
sundhar <32140112+mindaccentin@users.noreply.github.com>
2022-11-08 11:44:52 +0530

    javascriptbasic

---
+++ basic_javascript/frist.js
@@ -0,0 +1,41 @@
+var app_name = "Todo List";
+var task_1 = "Create a New Javascript Project";
+var task_status = "New"
+
+// Operators
+
+// 1.Assignment Operator (=) 
+
+var x = 10;
+document.getElementById("assign").innerHTML = x;
+
+// 2.  Addition Operator (+)
+
+var banana = 10;
+var apple = 15;
+var totalCast = banana + apple;
+
+document.getElementById("add").innerHTML = totalCast;
+
+
+// 3. Multiplication Operator (*)
+
+var x = 20;
+var y = 25;
+var z = x * y
+
+document.getElementById("mul").innerHTML = z;
+
+
+
+// Condition Statements
+
+// IF Else 
+
+// Looping
+
+// For
+
+// While
+
+// Switch case
